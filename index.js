3539f3cac19c967a9c6eabc1109f77d86c53ed69
Heljhum Enad <heljhum.enad@accenture.com>
2021-10-31 13:13:43 +0800

    Building server to listen to a specific port

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-//todo: build the sample application uptime checker using nodejs
-//node masterclass skillshare l
+// primary file for the API
+
+//Node Dependencies
+let http = require('http');
+
+// server responds for all query string
+let httpserver = http.createServer(function (req, res) {
+    res.end('Hello World\n');
+});
+
+// start the server and listen to port 30000
+httpserver.listen(3000, function () {
+    console.log("The Server is listen on 3000..");
+});
