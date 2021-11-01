51ef85c51bd3b1de1c105db66686f828fc6fb7a3
Heljhum Enad <heljhum.enad@accenture.com>
2021-11-01 22:26:08 +0800

    build and header format that accept handlers and output status code

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ let httpserver = http.createServer(function (req, res) {
     let stringDecoder = new stringdecoder('utf-8');
     // create variable that will hold the stream of data
     let buffer = '';
-    
+
     //create callback function to accept and append the stream of data from the http server
     req.on('data', function (data) {
         buffer += stringDecoder.write(data);
@@ -42,16 +42,63 @@ let httpserver = http.createServer(function (req, res) {
     //corelated all the stream of data if http server finish sending of stream of data
     req.on('end', function () {
         buffer += stringDecoder.end();
-        //send the response
-        res.end('Hello World\n');
-        //log the request path
-        // console.log('Request has been recieved on this path:' + trim_path + ' with this method:' + method + ' query string parameters:', queryobject);
-        console.log('Request has recieved header with this payload:', buffer);
-        console.log('type of:', typeof buffer)
-    })
+
+        //choose the handler this request should go. if not found call not found handler
+        let choosenHandler = typeof (router[trim_path]) !== 'undefined' ? router[trim_path] : handlers.notfound;
+
+        //construct the data object that will be send to the handler
+        let data = {
+            'trim_path': trim_path,
+            'queryobject': queryobject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+
+        //route the request to the handler specified in the router
+        choosenHandler(data, function (statusCode, payload) {
+            //use the status code called back by the handler or default to 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            //use the payload called back by the handler or default to an empty object
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            //convert the payload to a string
+            payload_string = JSON.stringify(payload);
+
+            //return the response as json content type
+            res.setHeader('Content-Type','application/json');
+            //return the response
+            res.writeHead(statusCode);
+            res.end(payload_string);
+            console.log('Returning the response:', statusCode, payload_string);
+
+
+        });
+    });
 });
 
 // start the server and listen to port 30000
 httpserver.listen(port, function () {
     console.log("The Server is listen on port " + port);
 });
+
+//define handlers
+let handlers = {};
+
+//sample handler
+handlers.sample = function (data, callback) {
+    //callback an http.status code payload should be a object
+    callback(406, { 'name': 'sample hanlder' });
+};
+
+//not found handlers
+handlers.notfound = function (data, callback) {
+    //callback an http status 404 but doesn't need an payload
+    callback(404);
+};
+//define a request router object
+let router = {
+    'sample': handlers.sample
+};
